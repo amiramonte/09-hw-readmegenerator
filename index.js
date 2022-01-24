2ce08407f7dd1dce3ee6eb6b9cfb869e8f3330c7
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-01-24 15:16:28 -0800

    added homework readme file to reference for hw completion, added index.js file, added package.json file, downloaded inquirer and created package-lock.json file, linked inquirer and fs to index.js file and tested inquirer connection and node index.js responsivness

---
+++ index.js
@@ -0,0 +1,50 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+
+inquirer.prompt ([
+    {
+        type: 'input',
+        message: 'Who is your daddy and what does he do?',
+        name: 'userName',
+    }
+])
+
+.then ((answers) => {
+    console.log(answers)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
